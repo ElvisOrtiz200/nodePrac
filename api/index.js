278e09544276f564456596052c8594980fef3145
ElvisOrtiz200 <170133104+ElvisOrtiz200@users.noreply.github.com>
2025-01-03 18:14:19 -0500

    finished API

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 
 const express = require('express')
 
-const {IncidentCreate} = require('../controllers/incident')
+const {IncidentCreate, getIncidentbyID, getIncident, deleteIncident, updateIncident} = require('../controllers/incident')
 const {solucion} = require('../controllers/solucion')
 
 const routes = express.Router();
 
+routes.post('/incident', IncidentCreate);
+routes.get('/incident/:id', getIncidentbyID);
+routes.get('/incidents', getIncident);
+routes.delete('/incident/:id', deleteIncident);
+routes.put('/incident/:id', updateIncident);
 
 
-
-routes.post('/Incidente',IncidentCreate)
 routes.get('/solucion',solucion)
 
-
+ 
 
 module.exports = {
     routes
--- controllers/incident.js
+++ controllers/incident.js
@@ -11,8 +11,70 @@ const IncidentCreate = async (req, res ) =>{
     }
 }
 
+const getIncident = async (req, res) => {
+    try {
+        const incidents = await Incident.find().sort({createdAt: 1})
+        res.status(200).json({ message: 'Incidentes obtenidos', data: incidents }); 
+    } catch (error) {
+        res.status(400).json({message: 'Error', error})
+    }
+}
+
+
+const getIncidentbyID = async(req, res) => {
+    try {
+        const { id } = req.params; // Obtener el ID desde los parámetros de la URL
+        const incidentFound = await Incident.findById(id); 
+        if (!incidentFound) {
+            return res.status(404).json({ message: 'Incidente no encontrado' }); // Responder si no se encuentra
+        }
+        res.status(200).json({message: 'Encontrado', data: incidentFound})
+    } catch (error) {
+        res.status(400).json({message:'Error',error})
+    }
+}
+
+const updateIncident = async (req, res) =>{
+    try {
+        const id = req.params.id;
+        const updateData = req.body;
+        const incident = await Incident.findByIdAndUpdate(id, updateData, {new: true});
+        if (!incident) {
+            return res.status(404).json({ message: 'Incidente no encontrado' });
+        }
+        res.status(200).json({message: 'Update', data: incident})
+    } catch (error) {
+        res.status(400).json({message: 'Error', error})
+    }
+}
+
+const deleteIncident = async (req, res) => {
+    try {
+        const { id } = req.params; // Obtener el ID desde los parámetros de la URL
+
+        // Eliminar el incidente por su ID
+        const incidentDelete = await Incident.findByIdAndDelete(id);
+
+        if (!incidentDelete) {
+            return res.status(404).json({ message: 'Incidente no encontrado' }); // Si no se encuentra el incidente, se responde con 404
+        }
+
+        // Si se encuentra y se elimina, se devuelve la respuesta de éxito
+        res.status(200).json({ message: 'Incidente eliminado', data: incidentDelete });
+    } catch (error) {
+        // En caso de error en la operación, respondemos con 400
+        res.status(400).json({ message: 'Error al eliminar el incidente', error });
+    }
+};
+
+
+
 module.exports = {
-    IncidentCreate
+    IncidentCreate,
+    getIncident,
+    getIncidentbyID,
+    updateIncident,
+    deleteIncident
 };
 
  
