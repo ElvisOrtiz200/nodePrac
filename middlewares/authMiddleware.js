c5ac573003a83af931708d7150cbaa99a9774f22
ElvisOrtiz200 <170133104+ElvisOrtiz200@users.noreply.github.com>
2025-01-05 23:08:48 -0500

    add comments

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
 const jwt = require('jsonwebtoken');
 
-
+//configurar authorization en el header para que funcione y se verifique
 const authenticateToken = (req, res, next) =>{
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
         return res.status(403).json({ message: 'Acceso denegado, no se proporcionó el token' });
     }
-    
+    //si el token vencio envia el error
     jwt.verify(token, SECRET_KEY, (err, user) =>{
         if(err){
             return res.status(403).json({ message: 'Token inválido' });
